634e5d6b38f147c5d715ccee0f0960377b32e52e
Willow Maui García <wmgm1998@gmail.com>
2025-04-10 16:53:32 +0200

    fix(#6Insercion_de_datos): Añadido LIMIT 1 a la consulta JPQL para asegurar un único resultado
    Se ha añadido LIMIT 1 a la consulta JPQL en PriceRepository para garantizar que solo se devuelva un único resultado al buscar precios por criterios. Esto corrige un posible problema donde se podrían devolver múltiples resultados, lo cual no es el comportamiento esperado.

--- infrastructure/src/main/java/org/example/infrastructure/repositories/jpa/PriceJpaRepository.java
+++ infrastructure/src/main/java/org/example/infrastructure/repositories/jpa/PriceJpaRepository.java
@@ -15,7 +15,7 @@ public interface PriceJpaRepository extends JpaRepository<PriceEntity, Long> {
     @Query("SELECT p " +
             "FROM PriceEntity p " +
             "WHERE p.startDate <= :fechaConsulta AND p.endDate >= :fechaConsulta AND p.productId = :productId AND p.brandId = :brandId " +
-            "ORDER BY p.priority DESC, p.startDate DESC")
+            "ORDER BY p.priority DESC, p.startDate DESC LIMIT 1")
     PriceEntity findPriceEntityByCriteria(
             @Param("fechaConsulta") Date fechaConsulta,
             @Param("productId") Long productId,
